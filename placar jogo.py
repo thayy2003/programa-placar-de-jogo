fe0876424287727b109724a3967fa478e0f21d30
thais vitória santos da silva <141461398+thayy2003@users.noreply.github.com>
2023-08-14 19:30:33 -0300

    Add files via upload
    4 timel = int (input("Palmeiras: " )) Д time2- int(input("corinthians" )) 
    5 print (f'Palmeiras(timel)-(time2) Corinthians.") 
    6- if (timel > time2) : 
    7 print ("Palmeiras ganhou: 
    8 - elif (timel < time2): ") 
    9 print ("corinthians ganhou: 
    10 - elif (timel =- time2 ) ") 
    11 print ("empate:

---
+++ placar jogo.py
@@ -0,0 +1,8 @@
+4 timel = int (input("Palmeiras: " )) Д time2- int(input("corinthians" )) 
+5 print (f'Palmeiras(timel)-(time2) Corinthians.") 
+6- if (timel > time2) : 
+7 print ("Palmeiras ganhou: 
+8 - elif (timel < time2): ") 
+9 print ("corinthians ganhou: 
+10 - elif (timel =- time2 ) ") 
+11 print ("empate:
